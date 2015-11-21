97fdc447886d10f6c963a3512873b3b11363f6c5
“Alvin <avinoz@gmail.com>
2015-11-21 11:57:54 -0800

    adds map display

---
+++ ningaloo/client/desktop/desktop.js
@@ -0,0 +1,22 @@
+var MAP_ZOOM = 15;
+
+Meteor.startup(function() {
+  GoogleMaps.load();
+});
+
+Template.map.helpers({
+  geolocationError: function() {
+    var error = Geolocation.error();
+    return error && error.message;
+  },
+  mapOptions: function() {
+    var latLng = Geolocation.latLng();
+    // Initialize the map once we have the latLng.
+    if (GoogleMaps.loaded() && latLng) {
+      return {
+        center: new google.maps.LatLng(latLng.lat, latLng.lng),
+        zoom: MAP_ZOOM
+      };
+    }
+  }
+});
