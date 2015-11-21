d0163f4d4b5a3db63056f85998c8704f89678d68
jato <jatobrown@gmail.com>
2015-11-20 21:09:38 -0800

    Turns data from forms and current location into mongo objects; appends to form page

--- ningaloo/client/form/form.js
+++ ningaloo/client/form/form.js
@@ -26,6 +26,45 @@ if (Meteor.isClient) {
 
 // ( "#test option:selected" ).text();
 
+// ########## TESTING THE MAP CALL
+  // Template.map.helpers({
+  //   geolocationError: function() {
+  //     var error = Geolocation.error();
+  //     return error && error.message;
+  //   },
+  //   mapOptions: function() {
+  //     let latLng = Geolocation.latLng();
+    
+  //     var pulse1 = JSON.stringify(latLng.lat)
+  //     var pulse2 = JSON.stringify(latLng.lng)
+  //     console.log(pulse1, pulse2)
+
+  //     console.log(latLng)
+  //     // Initialize the map once we have the latLng.
+  //     if (GoogleMaps.loaded() && latLng) {
+  //       return {
+  //         center: new google.maps.LatLng(latLng.lat, latLng.lng),
+  //         zoom: MAP_ZOOM
+  //       };
+  //     }
+  //   }
+  // });
+
+  // Template.map.events({
+  //   'submit form': function(event){
+  //     event.preventDefault();
+  //     console.log(pulse) 
+  //        $('.map_append').append(pulse1)
+
+  //   }
+  // });
+
+
+
+// ###############
+
+
+
 
   Template.form.events({
     'submit form': function(event){
@@ -34,13 +73,24 @@ if (Meteor.isClient) {
       var field2 = $( "#form_select2 option:selected" ).text();
       var field3 = $( "#form_select3 option:selected" ).text();
       var field4 = $( "#form_select4 option:selected" ).text();
+      var field5 = $( '#lat' ).text();
+      var field6 = $( '#lon' ).text();
       console.log("Form submitted");
       console.log(event.type);
       console.log(field1, field2, field3, field4);
 
+      $('.form_append1').append(field1)
+      $('.form_append2').append(field2)
+      $('.form_append3').append(field3)
+      $('.form_append4').append(field4)
 
 
-      var turtlelog = {division: field1}
+      // ### CREATES K/V OBJECT 
+      var turtlelog = {division: field1, section: field2, subsection: field3, turtleSpecies: field4, latLng:{lat:field5,lon:field6}}
+      console.log(turtlelog)
+
+
+
     },
     "change #form_select1":function(e){
       var division = $( "#form_select1 option:selected" ).text();
@@ -55,6 +105,7 @@ if (Meteor.isClient) {
       Session.set("subsection",subsection)
     }
   });
+
 
 /// DATA TEMP
 
