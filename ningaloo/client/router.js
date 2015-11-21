a062efbaae75cae04b0df9c99b6fab8aa61fe03d
Jose Taveras <josejuanisaac@gmail.com>
2015-11-21 14:52:10 -0800

    indivitual page testing

--- ningaloo/client/router.js
+++ ningaloo/client/router.js
@@ -7,5 +7,10 @@ Router.map(function(){
   this.route('form', {path: '/form'});
   this.route('list', {path: '/list'});
   this.route('map', {path: '/map'});
-  this.route('itempage', {path: '/itempage'});
+  // this.route('itempage',{path:'/itempage/id=:_id'} );
 });
+Router.route('/itempage/:_id', function () {
+  console.log("hey from route definition");
+  var item = Items.findOne({_id: this.params._id});
+  this.render('itempage', {data: item});
+})
