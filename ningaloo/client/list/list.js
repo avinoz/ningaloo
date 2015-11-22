8336539e0511fba5f5c34c1500ec35b3898d1ec7
“Alvin <avinoz@gmail.com>
2015-11-21 18:11:08 -0800

    testing photo to work on ios

--- ningaloo/client/list/list.js
+++ ningaloo/client/list/list.js
@@ -1,22 +1,6 @@
+console.log("Hello from list.js");
 Template.list.helpers({
     tasks: function (e) {
-            // Show newest tasks at the top
-      console.log(this);
-      console.log(e);
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
-
-Template.task.events({
-  "click a.item":function(e){
-    console.log(this);
-    e.preventDefault();
-    console.log(e.currentTarget);
-    Router.go("/itempage/"+this._id)
-  }
-});
-
-Template.registerHelper('fromNow', function(date) {
-  if (date)
-    return moment(date).fromNow(true);
-});
