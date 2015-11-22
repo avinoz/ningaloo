b89372c22e8b6e6876039b02aa74fba071c8f8df
“Alvin <avinoz@gmail.com>
2015-11-21 21:56:13 -0800

    swaps out classic map for a custom map and refractors map code

--- ningaloo/client/map/map.js
+++ ningaloo/client/map/map.js
@@ -1,54 +1,62 @@
 
-// Mapbox.load();
-// Tracker.autorun(function () {
-//   if (Mapbox.loaded()) {
-
-//     Meteor.call('getMapBoxKey', function(error, result){
-//       if (!error) {
-//         L.mapbox.accessToken = result
-//         let map = L.mapbox.map('map', 'avinoz.o11688nh');
-//       } else {
-//         console.log(error)
-//       }
-//     })
-//   }
-// });
-
-
-// Template.map.helpers({
-//   geolocationError: function() {
-//     var error = Geolocation.error();
-//     return error && error.message;
-//   },
-//   mapOptions: function() {
-//     let latLng = Geolocation.latLng();
-//     var pulse = JSON.stringify(latLng)
-
-//     $('#lat').html(latLng.lat.toFixed(6))
-//     $('#lon').html(latLng.lng.toFixed(6))
-//     $('#coord_cont').html("Current Coordinates")
-
-//     console.log(latLng)
-
-//     // INITIALIZE THE MAP WHEN WE HAVE COORDS
-//     if (GoogleMaps.loaded() && latLng) {
-//       return {
-//         center: new google.maps.LatLng(latLng.lat, latLng.lng),
-//         // zoom: MAP_ZOOM
-//       };
-//     }
-//   }
-// });
-
-
-////////////
 Mapbox.load();
-Tracker.autorun(function () {
-  if (Mapbox.loaded()) {
-    L.mapbox.accessToken = 'pk.eyJ1IjoiamF0bzQiLCJhIjoiY2loOXFma2F6MHUyZ3Uwa2xrbGc3eTUzNSJ9.A_hh1IRqqzuoytmTkJ-2GQ';
-    var map = L.mapbox.map('map', 'mapbox.streets');
-  }
+
+Template.map.onRendered(function () {
+  Mapbox.debug = true;
+  Mapbox.load({
+    plugins: ['Leaflet']
+  });
+
+  this.autorun(function () {
+    if (Mapbox.loaded()) {
+
+      Meteor.call('getMapBoxKey', function(error, result){
+        if (!error) {
+          L.mapbox.accessToken = result
+          let map = L.mapbox.map('map', 'avinoz.o7nj93k2');
+          map.setView([-21.854578, 114.103581], 12)
+          // ADDS PIN TO MAP WITH HARD-CODE LAT/LNG
+          L.marker([-21.847727, 114.033028]).addTo(map);
+          L.marker([-21.830199, 114.062726]).addTo(map);
+          L.marker([-21.834502, 114.054314]).addTo(map);
+          L.marker([-21.810967, 114.091414]).addTo(map);
+          L.marker([-21.880068, 113.995091]).addTo(map);
+          // L.market([latLng.lat, latLng.lng])
+        } else {
+          console.log(error)
+        }
+      })
+
+      // ##### JSON STYLE OPTION FOR ADDING A PIN TO THE MAP
+      // L.mapbox.featureLayer({
+      //   type: 'Feature',
+      //   geometry: {
+      //     type: 'Point',
+      //     coordinates: [
+      //     -22.03501,
+      //     113.54410
+      //     ]
+      //   },
+
+      //   properties: {
+      //     title: 'Ningaloo',
+      //     description: '',
+      //     'marker-size': 'large',
+      //     'marker-color': '#BE9A6B',
+      //     'marker-symbol': 'default'
+      //   }
+      // }).addto(map)
+    }
+  });
+
+  this.autorun(function () {
+    if (Mapbox.loaded()) {
+      geojson = Tasks.find().fetch()
+      view.featureLayer.setGeoJSON(geojson);
+    }
+  });
 });
+
 
 Template.map.helpers({
   geolocationError: function() {
@@ -76,55 +84,6 @@ Template.map.helpers({
       };
     }
   }
-});
-
-Template.map.onRendered(function () {
-  Mapbox.debug = true;
-  Mapbox.load({
-    plugins: ['markercluster', 'heat', 'Leaflet']
-  });
-
-  this.autorun(function () {
-    if (Mapbox.loaded()) {
-      L.mapbox.accessToken = 'pk.eyJ1IjoiamF0bzQiLCJhIjoiY2loOXFma2F6MHUyZ3Uwa2xrbGc3eTUzNSJ9.A_hh1IRqqzuoytmTkJ-2GQ';
-      var map = L.mapbox.map('map', 'mapbox.streets')
-      var view = map.setView([-22.03501, 113.54410], 6)
-
-      // ADDS PIN TO MAP WITH HARD-CODE LAT/LNG ################
-      L.marker([-22.0, 113.5]).addTo(map);
-      L.marker([-22.0, 114.5]).addTo(map);
-      L.marker([37.775408,-122.413682]).addTo(map);
-      L.market([latLng.lat, latLng.lng])
-
-      // ##### JSON STYLE OPTION FOR ADDING A PIN TO THE MAP
-      // L.mapbox.featureLayer({
-      //   type: 'Feature',
-      //   geometry: {
-      //     type: 'Point',
-      //     coordinates: [
-      //     -22.03501,
-      //     113.54410
-      //     ]
-      //   },
-
-      //   properties: {
-      //     title: 'Ningaloo',
-      //     description: '',
-      //     'marker-size': 'large',
-      //     'marker-color': '#BE9A6B',
-      //     'marker-symbol': 'default'
-      //   }
-      // }).addto(map)
-      // -22.03501,113.54410
-    }
-  });
-
-  this.autorun(function () {
-    if (Mapbox.loaded()) {
-      geojson = Tasks.find().fetch()
-      view.featureLayer.setGeoJSON(geojson);
-    }
-  });
 });
 
 
