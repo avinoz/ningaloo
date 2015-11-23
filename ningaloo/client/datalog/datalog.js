b94bce4b93a5d1eb703a6825bf8fd431122135a8
stacielove <stacie@love.com>
2015-11-22 18:50:46 -0800

    Adds class to table row to link to associated item entry page.

--- ningaloo/client/datalog/datalog.js
+++ ningaloo/client/datalog/datalog.js
@@ -1,3 +1,13 @@
 Template.datalog.turtlelogs = function() {
-  return TurtleLogs.find(); 
+  return TurtleLogs.find({}, {limit:100}); 
 }
+
+Template.datalog.events({
+  "click .clickable-row":function(e){
+    console.log(this);
+    e.preventDefault();
+    console.log(e.currentTarget);
+    Router.go("/itempage/"+this._id)
+  }
+});
+
