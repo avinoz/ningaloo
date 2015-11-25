fbbfc246597e92c8ad9f358dad503a3be0e01bea
jato <jatobrown@gmail.com>
2015-11-24 21:35:44 -0800

    Displays data for logs inline with div

--- ningaloo/client/desktop/desktop.js
+++ ningaloo/client/desktop/desktop.js
@@ -211,56 +211,57 @@ Meteor.subscribe("turtlelogs");
 Template.desktop.helpers({
     turtlelogs: function (e) {  //change in list.html
       return TurtleLogs.find({}, {limit:10});
-    },
-    sortLogs:function(field,order){ //change in list.html
-      var orderString = order>0?"asc":"desc";
-      var o = {}
-      o[field]=order;
-      var sort = {sort:o};
-      return TurtleLogs.find({},sort);
-
-    },
-    sortingBy:function(){
-      return Session.get("sortingBy");
-    },
-    sortOrder:function(){
-      return Session.get("sortOrder")||1;
     }
+    // sortLogs:function(field,order){ //change in list.html
+    //   var orderString = order>0?"asc":"desc";
+    //   var o = {}
+    //   o[field]=order;
+    //   var sort = {sort:o};
+    //   return TurtleLogs.find({},sort);
+
+    // },
+    // sortingBy:function(){
+    //   return Session.get("sortingBy");
+    // },
+    // sortOrder:function(){
+    //   return Session.get("sortOrder")||1;
+    // }
   });
-Template.sortingFields.events({
-  "change #sortingSelect":function(e){
-    var sortingBy = $( "#sortingSelect option:selected" ).text();
-    if(sortingBy.length==0){
-      Session.set("sortingBy",undefined);
-    }else{
-      Session.set("sortingBy",sortingBy);
-    }
-  },
-  "click #sortOrder":function(){
-      if(Session.get("sortOrder")){
-        Session.set("sortOrder",-Session.get("sortOrder"));
-      }else{
-        Session.set("sortOrder",-1)
-      }
-    }
-});
-Session.set("sortingBy","date");
-Template.sortingFields.helpers({
-  fields:[
-    {fieldName:"date"},
-    {fieldName:"division"},
-    {fieldName:"section"},
-    {fieldName:"subsection"},
-    {fieldName:"species"}
+// Template.sortingFields.events({
+//   "change #sortingSelect":function(e){
+//     var sortingBy = $( "#sortingSelect option:selected" ).text();
+//     if(sortingBy.length==0){
+//       Session.set("sortingBy",undefined);
+//     }else{
+//       Session.set("sortingBy",sortingBy);
+//     }
+//   },
+//   "click #sortOrder":function(){
+//       if(Session.get("sortOrder")){
+//         Session.set("sortOrder",-Session.get("sortOrder"));
+//       }else{
+//         Session.set("sortOrder",-1)
+//       }
+//     }
+// });
+// Session.set("sortingBy","date");
+// Template.sortingFields.helpers({
+//   fields:[
+//     {fieldName:"date"},
+//     {fieldName:"division"},
+//     {fieldName:"section"},
+//     {fieldName:"subsection"},
+//     {fieldName:"species"}
 
-  ]
-});
+//   ]
+// });
 
 
 
 Template.desktop.events({
-  "click a":function (e) {
+  "click #turtle-list":function (e) {
     e.preventDefault();
+    this.$(".toggle-row").toggle();
     console.log(e.target.href);
     Router.go('/itempage/'+this._id);
   }
@@ -268,6 +269,7 @@ Template.desktop.events({
 
 
 Template.desktop.turtlelogs = function () {
+  var result = HTTP.call("GET", '/itempage/'+this._id)
   return Session.get('result');
 };
 
