533157447193c8ce87b470794cf1a0c6092de236
stacielove <stacie@love.com>
2015-11-26 14:16:52 -0800

    Created partial files for fuzzy search and added notes category to js for search string.

--- ningaloo/client/desktop/fuzzysearch.js
+++ ningaloo/client/desktop/fuzzysearch.js
@@ -1,45 +1,9 @@
-// var options = {
-//   keepHistory: 1000 * 60 * 5,
-//   localSearch: true
-// };
-// var fields = ['packageName', 'description'];
-
-// PackageSearch = new SearchSource('packages', fields, options);
-
-// Template.searchResult.helpers({
-//   getPackages: function() {
-//     return PackageSearch.getData({
-//       transform: function(matchText, regExp) {
-//         return matchText.replace(regExp, "<b>$&</b>")
-//       },
-//       sort: {isoScore: -1}
-//     });
-//   },
-  
-//   isLoading: function() {
-//     return PackageSearch.getStatus().loading;
-//   }
-// });
-
-// Template.searchResult.rendered = function() {
-//   PackageSearch.search('');
-// };
-
-// Template.searchBox.events({
-//   "keyup #search-box": _.throttle(function(e) {
-//     var text = $(e.target).val().trim();
-//     PackageSearch.search(text);
-//   }, 200)
-// });
-
-// Turtle Logs
 var options = {
   keepHistory: 1000 * 60 * 5,
   localSearch: true
 };
-// var fields = ['division', 'section', 'subsection', 'species', 'nest_ID'];
 
-TurtleSearch = new SearchSource('turtlelogs', ['division'], options);
+TurtleSearch = new SearchSource('turtlelogs', ['division','species','section','subsection','notes'], options);
 
 Template.searchResult.helpers({
   getTurtles: function() {
