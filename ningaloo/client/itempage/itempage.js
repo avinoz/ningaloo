20bd45e268955fe6e03200f7cf1e3be327642fcb
Jose Taveras <josejuanisaac@gmail.com>
2015-11-27 04:24:15 -0800

    Fix itempage and indiview

--- ningaloo/client/itempage/itempage.js
+++ ningaloo/client/itempage/itempage.js
@@ -1,10 +1,11 @@
 Meteor.subscribe("images");
-Meteor.subscribe("turtlelogs");
+Meteor.subscribe("turtlelog");
 Template.imageView.helpers({
   images: function () {
     return Images.find(); // Where Images is an FS.Collection instance
   },
   getImage:function(id){
+    console.log("Trying to find image...");
     var result = Images.find(id);
     return result;
   }
